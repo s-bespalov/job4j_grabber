5c54f308bfd582dd5b54b0b448a6548b1354a3ad
Sergey Bespalov <sergey.bespalov@protonmail.com>
2024-03-30 23:26:51 +0300

    2. Парсинг HTML страницы. [#260358]

---
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -0,0 +1,34 @@
+package ru.job4j.grabber;
+
+import org.jsoup.Jsoup;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+
+public class HabrCareerParse {
+
+    private static final String SOURCE_LINK = "https://career.habr.com";
+    private static final String PREFIX = "/vacancies?page=";
+    private static final String SUFFIX = "&q=Java%20developer&type=all";
+
+    public static void main(String[] args) throws IOException {
+        var pageNumber = 1;
+        var fullLink = "%s%s%d%s".formatted(SOURCE_LINK, PREFIX, pageNumber, SUFFIX);
+        var connection = Jsoup.connect(fullLink);
+        var document = connection.get();
+        var rows = document.select(".vacancy-card__inner");
+        rows.forEach(row -> {
+            var titleElement = row.select(".vacancy-card__title").first();
+            var linkElement = Objects.requireNonNull(titleElement).child(0);
+            var vacancyName = titleElement.text();
+            var link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+            var dateElement = row.select(".vacancy-card__date").first();
+            var timeElement = Objects.requireNonNull(dateElement).child(0);
+            var timestamp = LocalDateTime.parse(
+                    timeElement.attr("datetime"), DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+            System.out.printf("%s %s %s %n", vacancyName, link, timestamp.format(DateTimeFormatter.ISO_DATE));
+        });
+    }
+}
